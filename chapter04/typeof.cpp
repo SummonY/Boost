0870b50d1b534c848497496a51f941d3af53dd17
Summon Yang <SummonYang1991@gmail.com>
2015-07-19 14:31:56 +0800

    typeof demo.

--- chapter04/typeof.cpp
+++ chapter04/typeof.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <boost/typeof/typeof.hpp>
 #include <typeinfo>
+#include <boost/make_shared.hpp>
 #include BOOST_TYPEOF_INCREMENT_REGISTRATION_GROUP()
 
 namespace ex
@@ -22,6 +23,8 @@ vector<string> func()
     return v;
 }
 
+#define auto_t      BOOST_AUTO
+
 
 int main()
 {
@@ -37,5 +40,8 @@ int main()
     x2.second.a = 10;
     x2.second.b = 20;
     cout << x2.second.a << " " << x2.second.b << endl;
+
+    auto_t(p2, make_shared<int>(1000));
+    cout << *p2 << endl;
     return 0;
 }
